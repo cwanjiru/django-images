a7c18251c36749c7a75d87198d8ab35e0192cd47
christine <gacuirichristine@gmail.com>
2020-06-04 12:09:03 +0300

    Define edit view function that allows users to edit their profile details and account details.

--- account/views.py
+++ account/views.py
@@ -9,11 +9,23 @@ from .models import Profile
 # Create your views here.
 @login_required
 def edit(request):
+    user_profile = Profile.objects.get(user=request.user)
     if request.method == 'POST':
-        user_form = UserEditForm()
-
+        user_form = UserEditForm(instance=request.user,
+                                data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user, 
+                                        data=request.POST,
+                                        files=request.files)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save() 
+            profile_form.save()
     else:
-        pass
+       user_form = UserEditForm(instance=request.user)
+       profile_form = ProfileEditForm(instance=user_profile)
+    return render(request,
+                    'account/edit.html', 
+                    {'user_form':user_form, 
+                    'profile_form':profile_form})
 
 @login_required
 def dashboard(request):
