2aa76bbea01c5661efa7bfe5b7fc86e278ee124e
christine <gacuirichristine@gmail.com>
2020-06-29 16:04:04 +0300

    Update the value first to the instance of EditForm to user_profile.

--- account/views.py
+++ account/views.py
@@ -13,9 +13,9 @@ def edit(request):
     if request.method == 'POST':
         user_form = UserEditForm(instance=request.user,
                                 data=request.POST)
-        profile_form = ProfileEditForm(instance=request.user, 
+        profile_form = ProfileEditForm(instance=user_profile, 
                                         data=request.POST,
-                                        files=request.files)
+                                        files=request.FILES)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save() 
             profile_form.save()
