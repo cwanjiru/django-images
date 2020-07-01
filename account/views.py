089f400753435150028464344b1d6cf992254d10
christine <gacuirichristine@gmail.com>
2020-07-01 15:49:30 +0300

    Use the messages framework to notify the user whether the profile has been successfully updated or has failed.

--- account/views.py
+++ account/views.py
@@ -5,6 +5,7 @@ from .forms import LoginForm, UserRegistrationForm, \
                     UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+from django.contrib import messages
 
 # Create your views here.
 @login_required
@@ -19,6 +20,11 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save() 
             profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+        
+        else:
+            messages.error(request, 'Error updating your profile')
+
     else:
        user_form = UserEditForm(instance=request.user)
        profile_form = ProfileEditForm(instance=user_profile)
